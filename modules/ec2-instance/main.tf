6fa59750abd3c9782ad564e5d3ea2f7ae044ea65
Trần Quốc Tú <tuqt@MacBook-Pro-cua-Tran.local>
2026-03-31 18:14:20 +0700

    Update 4

--- modules/ec2-instance/main.tf
+++ modules/ec2-instance/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "app" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
-  key_name      = var.key_name
 
   user_data = file("${path.module}/scripts/install_docker.sh")
 
--- modules/ec2-instance/variables.tf
+++ modules/ec2-instance/variables.tf
@@ -2,6 +2,5 @@ variable "ami_id" {}
 variable "instance_type" {}
 variable "subnet_id" {}
 variable "security_group_id" {}
-variable "key_name" {}
 variable "env" {}
 variable "common_tags" { type = map(string) }
