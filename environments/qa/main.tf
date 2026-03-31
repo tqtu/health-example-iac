49ef09f4e6679721b6a01da21daa3c25c225496b
Trần Quốc Tú <tuqt@MacBook-Pro-cua-Tran.local>
2026-03-31 18:02:20 +0700

    Update 2

--- environments/qa/main.tf
+++ environments/qa/main.tf
@@ -1,5 +1,6 @@
 locals {
   env = "qa"
+
   common_tags = {
     Project     = "HealthApp"
     Environment = "qa"
@@ -7,6 +8,28 @@ locals {
   }
 }
 
+# =========================
+# DYNAMIC AMI (Ubuntu 24.04 - Canonical)
+# =========================
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# =========================
+# VPC
+# =========================
 module "vpc" {
   source      = "../../modules/vpc"
   env         = local.env
@@ -14,20 +37,26 @@ module "vpc" {
   common_tags = local.common_tags
 }
 
+# =========================
+# SECURITY GROUPS
+# =========================
 module "security_groups" {
   source = "../../modules/security-groups"
   env    = local.env
   vpc_id = module.vpc.vpc_id
 }
 
+# =========================
+# EC2 COMPUTE
+# =========================
 module "compute" {
   source = "../../modules/ec2-instance"
-  env    = local.env
-  # CHANGE THIS: This is the correct ID for Ubuntu 24.04 in Sydney (ap-southeast-2)
-  ami_id            = "ami-0310483fb2b4881ef"
-  instance_type     = "t3.micro"
-  key_name          = "key_learn_aws_instance_free"
-  subnet_id         = module.vpc.public_subnet_ids[0]
-  security_group_id = module.security_groups.web_sg_id
-  common_tags       = local.common_tags
+
+  env                = local.env
+  ami_id             = data.aws_ami.ubuntu.id
+  instance_type      = "t3.micro"
+  key_name           = "key_learn_aws_instance_free"
+  subnet_id          = module.vpc.public_subnet_ids[0]
+  security_group_id  = module.security_groups.web_sg_id
+  common_tags        = local.common_tags
 }
