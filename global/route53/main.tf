7ce9b132c3bcb64c5d950f8fab5edfaaa4ad3431
Trần Quốc Tú <tuqt@MacBook-Pro-cua-Tran.local>
2026-04-01 14:56:47 +0700

    all route 53

--- global/route53/main.tf
+++ global/route53/main.tf
@@ -1,17 +1,19 @@
 resource "aws_route53_record" "env_record" {
   zone_id = var.hosted_zone_id
 
-  # Logic: Creates 'qa.domain.com' or just 'domain.com'
+  # Logic: Creates 'qa.unboundshare.com' or 'unboundshare.com'
   name    = var.subdomain == "" ? var.domain_name : "${var.subdomain}.${var.domain_name}"
-  type    = "A"
 
-  alias {
-    name                   = var.s3_website_endpoint
-    zone_id                = var.s3_hosted_zone_id
-    evaluate_target_health = false
-  }
+  # Use CNAME for learning; it's easier when bucket names don't match
+  type    = "CNAME"
+  ttl     = "300"
+  records = [var.s3_website_endpoint]
+}
+
+data "aws_route53_zone" "selected" {
+  zone_id = var.hosted_zone_id
 }
 
 output "name_servers" {
-  value = var.hosted_zone_id # This is wrong, see fix below
+  value = data.aws_route53_zone.selected.name_servers
 }
--- global/route53/variables.tf
+++ global/route53/variables.tf
@@ -5,22 +5,26 @@ variable "domain_name" {
 
 variable "subdomain" {
   type        = string
-  description = "The prefix (e.g., 'qa' or 'www'). Empty for root."
+  description = "The prefix (e.g., 'qa' or 'www'). Empty string for the root domain."
 }
 
 variable "hosted_zone_id" {
   type        = string
-  description = "The ID from the aws_route53_zone resource"
+  description = "The ID of the Hosted Zone where this record will be created"
 }
 
 variable "s3_website_endpoint" {
   type        = string
+  description = "The website endpoint provided by the S3 bucket"
 }
 
 variable "s3_hosted_zone_id" {
   type        = string
+  description = "The specific Hosted Zone ID for the S3 bucket region (e.g., Z3O0J2DX0C6PQG for ap-southeast-1)"
 }
 
 variable "common_tags" {
-  type = map(string)
+  type        = map(string)
+  description = "A map of tags to apply to resources (Note: Route 53 Records do not support tags)"
+  default     = {}
 }
